f0239f8662ad6b3d243342c85329463075185c6f
Diegesis & Mimesis <diegesisandmimesis@proton.me>
2025-09-12 19:29:55 -0700

    Added dependency

--- actionBuffer.h
+++ actionBuffer.h
@@ -2,7 +2,13 @@
 // actionBuffer.h
 //
 
-// Uncomment to enable debugging options.
-//#define __DEBUG_ACTION_BUFFER
+#include "dataTypes.h"
+#ifndef DATA_TYPES_H
+#error "This module requires the dataTypes module."
+#error "https://github.com/diegesisandmimesis/dataTypes"
+#error "It should be in the same parent directory as this module.  So if"
+#error "actionBuffer is in /home/user/tads/actionBuffer, then"
+#error "dataTypes should be in /home/user/tads/dataTypes ."
+#endif // DATA_TYPES_H
 
 #define ACTION_BUFFER_H
